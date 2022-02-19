43056719a59bb8b285a0c8c84b0a7891183ab650
Nikita Nikitin <nikitanikitin2305@gmail.com>
2022-02-19 16:04:30 +0300

    nomsg

--- ACOS/3.tasks/1.s
+++
@@ -1,9 +0,0 @@
-    .text
-    .global f
-
-f:
-    mul x0, x0, x3
-    add x0, x0, x1
-    mul x0, x0, x3
-    add x0, x0, x2
-    ret
--- ACOS/4.tasks/q.c
+++
@@ -1,9 +0,0 @@
-long long R;
-
-void calculate() {
-    R = 5;
-}
-
-int main() {
-
-}
